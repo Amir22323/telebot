b84c38e782c5038aa8fbac3b566528e7a37e46e7
Amir22323 <131588675+Amir22323@users.noreply.github.com>
2023-04-30 13:21:48 +0300

    Add files via upload

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,6 +1,9 @@
 import telebot
 
+from weather import get_weather
+
 token = "5864188381:AAEO_QPsiC33SLo22sFOx-jeoIf6GBO8PZ8"
+weather_key = "7080b85d03b618cd74a84a0fa43249d3"
 
 bot = telebot.TeleBot(token)
 
@@ -11,27 +14,24 @@ def start(message):
 
 @bot.message_handler(commands=['help'])
 def start(message):
-    bot.send_message(message.chat.id, 'Чем помочь?')
+    bot.send_message(message.chat.id, 'Я ничего не умею :(')
 
 
 @bot.message_handler(content_types=['text'])
 def answer(message):
     if message.text.lower() == "привет":
         bot.send_message(message.chat.id, 'привет')
+    elif message.text.lower() == "погода":
+        city = "Пушкино"
+        data = get_weather(weather_key, city)
+        print(data)
+        message_answer = "Место:{city} \r\nТемпература:{temp}".format(city=data['name'],
+                                                    temp=data['main']['temp'])
+        bot.send_message(message.chat.id, message_answer)
     else:
         bot.send_message(message.chat.id, 'Я тебя не понял')
 
 
-@bot.message_handler(content_types=['text'])
-def answer(message):
-    if message.text.lower() == "как дела":
-        bot.send_message(message.chat.id, 'норм')
-
-
-@bot.message_handler(content_types=['text'])
-def answer(message):
-    if message.text.lower() == "расскажи анекдот":
-        bot.send_message(message.chat.id, 'ок')
 
 
 bot.infinity_polling()
