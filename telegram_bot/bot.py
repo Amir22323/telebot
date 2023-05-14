c11a8a1fe14c0a228eb9cd118b1044020c0f42d7
Amir22323 <131588675+Amir22323@users.noreply.github.com>
2023-05-14 13:22:34 +0300

    Add files via upload

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,37 +1,47 @@
 import telebot
+import json
 
-from weather import get_weather
-
+def parse_shedule(spisok):
+    result = ""
+    i = 1
+    for subject in spisok:
+        result += "{n} - {name}\r\n".format(n=i, name=str(subject))
+        i += 1
+    return result
 token = "5864188381:AAEO_QPsiC33SLo22sFOx-jeoIf6GBO8PZ8"
-weather_key = "7080b85d03b618cd74a84a0fa43249d3"
 
+with open("shedule.json", 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+print(data)
 bot = telebot.TeleBot(token)
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Привет')
-
-
-@bot.message_handler(commands=['help'])
-def start(message):
-    bot.send_message(message.chat.id, 'Я ничего не умею :(')
-
 
 @bot.message_handler(content_types=['text'])
 def answer(message):
-    if message.text.lower() == "привет":
-        bot.send_message(message.chat.id, 'привет')
-    elif message.text.lower() == "погода":
-        city = "Пушкино"
-        data = get_weather(weather_key, city)
-        print(data)
-        message_answer = "Место:{city} \r\nТемпература:{temp}".format(city=data['name'],
-                                                    temp=data['main']['temp'])
-        bot.send_message(message.chat.id, message_answer)
-    else:
-        bot.send_message(message.chat.id, 'Я тебя не понял')
+    if message.text.lower() == "понедельник":
+        bot.send_message(message.chat.id, parse_shedule(data["расписание"]["понедельник"]))
+    if message.text.lower() == "вторник":
+        bot.send_message(message.chat.id, parse_shedule(data["расписание"]["вторник"]))
+    if message.text.lower() == "среда":
+        bot.send_message(message.chat.id, parse_shedule(data["расписание"]["среда"]))
+    if message.text.lower() == "четверг":
+        bot.send_message(message.chat.id, parse_shedule(data["расписание"]["четверг"]))
+    if message.text.lower() == "пятница":
+        bot.send_message(message.chat.id, parse_shedule(data["расписание"]["пятница"]))
 
+    if message.text.lower() == "все расписание":
+        bot.send_message(message.chat.id, parse_shedule(data["расписание"]["понедельник"]) +
+            parse_shedule(data["расписание"]["вторник"]) +
+            parse_shedule(data["расписание"]["среда"]) +
+            parse_shedule(data["расписание"]["четверг"]) +
+            parse_shedule(data["расписание"]["пятница"]))
 
+    if message.text.lower() == "расписание":
+        keyboard = telebot.types.ReplyKeyboardMarkup()
+        keyboard.row("понедельник","вторник","среда")
+        keyboard.row("четверг","пятница","все расписание")
 
+        bot.send_message(message.chat.id, "выберите день", reply_markup=keyboard)
 
+print("start")
 bot.infinity_polling()
